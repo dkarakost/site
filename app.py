a7bfffd667d03e1712afae3a21a7e34088db9fea
Your Name <you@example.com>
2024-02-21 20:33:23 +0100

    Update app.py update files base.html,index.html, login.html

--- app.py
+++ app.py
@@ -4,17 +4,27 @@ from flask import Flask, render_template, url_for, request, flash,redirect
 app = Flask(__name__)
 app.secret_key ="mitsiomitsio"
 
-@app.route("/hello")
-def index():
+@app.route("/")
+def home():
     flash("What's your name")
-    return render_template("index.html")
+    return render_template("base.html")
 
 @app.route("/greet", methods=["POST","GET"])
 def greet():
     flash("Hello " + str(request.form['name_input'] + ", great to see you"))
-    return render_template("index.html")
+    return render_template("base.html")
+
+@app.route("/login", methods=["POST","GET"])
+def login():
+    return render_template("login.html")
+
+
+
+@app.route("/contact")
+def contact():
+    pass
 
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)#re-run the server
